29dba24d4fdd22bfda200a7eee204bdd6a232cd9
noahburrows <w0772238@myscc.ca>
2022-01-29 11:16:41 -0500

    Create interface and class

---
+++ N_Burrows_DarkSouls/src/app/helper-files/content-list.ts
@@ -0,0 +1,26 @@
+class ContentList {
+
+  private _items: Content[];
+
+  constructor(item: Content) {
+    this._items = []; // initialize array
+  }
+
+  get items(): Content[] {
+    return this._items;
+  }
+
+  addContent(newContent: Content) {
+    return this._items.push(newContent);
+  }
+
+  contentCount() {
+    return this._items.length;
+  }
+
+  processContent(index: number): String {
+    let content: Content = this._items[index];
+    return "Title: " + content.title + "\nDescription: " + content.description + "\nCreator: " + content.creator
+      + "\nImage: " + content.imgURL + "\nType: " + content.type;
+  }
+}
