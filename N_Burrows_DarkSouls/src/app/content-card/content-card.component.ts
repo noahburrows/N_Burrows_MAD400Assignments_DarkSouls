5eb6b61b8a07c38235a4fdf001589543fe7162c1
noahburrows <w0772238@myscc.ca>
2022-02-05 14:24:18 -0500

    Create component

--- N_Burrows_DarkSouls/src/app/content-card/content-card.component.ts
+++ N_Burrows_DarkSouls/src/app/content-card/content-card.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -11,17 +10,17 @@ export class ContentCardComponent implements OnInit {
 
   constructor() {
 
-    let stuffList: ContentList;
+    //let stuffList: ContentList;
 
-    stuffList = new ContentList();
+    //stuffList = new ContentList();
 
-    let ds1: Content = { id: 1, title: "First one", description: "This is the first Dark Souls", creator: "From Software" };
+    /*let ds1: Content = { id: 1, title: "First one", description: "This is the first Dark Souls", creator: "From Software" };
     let ds2: Content = { id: 2, title: "Second one", description: "The black sheep of the series", creator: "From Software" };
-    let ds3: Content = { id: 3, title: "Third one", description: "The grand finale", creator: "From Software" };
+    let ds3: Content = { id: 3, title: "Third one", description: "The grand finale", creator: "From Software" }; 
 
     stuffList.addContent(ds1)
     stuffList.addContent(ds2)
-    stuffList.addContent(ds3)
+    stuffList.addContent(ds3) */
 
     //console.log(stuffList.processContent(0));
 
