d4a3201a5a602060dea70f1610938321622e6fec
noahburrows <w0772238@myscc.ca>
2022-02-19 17:01:35 -0500

    Added bolding and underlining to directive

--- N_Burrows_DarkSouls/src/app/hover-affect.directive.ts
+++ N_Burrows_DarkSouls/src/app/hover-affect.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) {
+  }
+
+  @HostListener('mouseover')
+  onMouseOver() {
+    this.elem.nativeElement.style.textDecoration = 'underline';
+    this.elem.nativeElement.style.fontStyle = 'bold';
+  }
+
+  @HostListener('mouseout')
+  onMouseOut() {
+    this.elem.nativeElement.style.textDecoration = 'none';
+    this.elem.nativeElement.style.fontStyle = 'none';
+  }
 
 }
