6c77263503d370411605266d53efc475af3c66bd
noahburrows <w0772238@myscc.ca>
2022-02-19 16:40:45 -0500

    Create directive

--- N_Burrows_DarkSouls/src/app/app.module.ts
+++ N_Burrows_DarkSouls/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { FindPlatformPipe } from './find-platform.pipe';
+import { HoverAffectDirective } from './hover-affect.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
     ContentListComponent,
-    FindPlatformPipe
+    FindPlatformPipe,
+    HoverAffectDirective
   ],
   imports: [
     BrowserModule
