3b638aef28d1274b55c09071520d74cd273c7604
noahburrows <w0772238@myscc.ca>
2022-04-01 13:40:14 -0400

    Added theme

--- N_Burrows_DarkSouls/src/app/app.module.ts
+++ N_Burrows_DarkSouls/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientInMemoryWebApiModule } from
   "angular-in-memory-web-api";
 import { InMemoryDataService } from "./services/in-memory-data.service";
 import { ModifyContentComponentComponent } from './modify-content-component/modify-content-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { ModifyContentComponentComponent } from './modify-content-component/modi
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: false,
       delay: 500
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
